cf14e430d5f04261c67577640cf65434f949f50a
dic09005 <dic09005@byui.edu>
2022-12-08 20:28:41 -0700

    fix top

--- pong/game/scripting/collide_borders_action.py
+++ pong/game/scripting/collide_borders_action.py
@@ -26,9 +26,16 @@ class CollideBordersAction(Action):
             ball.bounce_x()
             self._audio_service.play_sound(bounce_sound)
 
-        if y < FIELD_TOP:
-            ball.bounce_y()
-            self._audio_service.play_sound(bounce_sound)
+        elif y <= (FIELD_TOP - BALL_WIDTH):
+            stats = cast.get_first_actor(STATS_GROUP)
+            stats.lose_life()
+            
+            if stats.get_lives() > 0:
+                callback.on_next(TRY_AGAIN) 
+            else:
+                callback.on_next(GAME_OVER)
+                self._audio_service.play_sound(over_sound)
+        
 
         elif y >= (FIELD_BOTTOM - BALL_WIDTH):
             stats = cast.get_first_actor(STATS_GROUP)
